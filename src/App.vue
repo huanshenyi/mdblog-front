c123c56a022ab4b37cf7734c70a821077a935d57
huanshenyi <txy1226052@gmail.com>
2020-08-21 01:10:43 +0900

    computedの使用

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
      <p>{{ name }}</p>
-     <p>{{ age }} <button type="button" @click="plusOne">+</button></p>
+     <p>
+       <button type="button" @click="changeAge(-1)">-</button>
+          年齢{{ age }}
+       <button type="button" @click="changeAge(1)">+</button>
+       </p>
+       <p>
+         <button type="button" @click="changeYear(-1)">-</button>
+         生まれ: {{ year }}
+         <button type="button" @click="changeYear(1)">+</button>
+       </p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 export default {
    setup() {
      const name = ref("abc");
      const age = ref(18);
-     function plusOne(){
-       age.value += 1;
+     const year = computed({
+       get: ()=> {
+          return 2020 - age.value     
+       },
+       
+       set: (val) => {
+         age.value =  2020 - val
+       }
+     });
+     function changeAge(val){
+       age.value += val;
      }
-     return {name, age, plusOne}
+     function changeYear(val){
+       year.value = year.value + val;
+     }
+     return {name, age, changeAge, year, changeYear}
    }
 }
 </script>
