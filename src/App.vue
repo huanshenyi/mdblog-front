078aaa34ebe4daf30d7fbc0778e08165c455b7b1
huanshenyi <txy1226052@gmail.com>
2020-08-28 16:17:56 +0900

    vue-routerとコンポネントの移動

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <section class="section">
     <div class="container">
-      <Suspense>
-        <template #default>
-          <timeline/>
-        </template>
-        <template #fallback>
-          <Progress/>
-        </template>
-      </Suspense>
+      <router-view></router-view>
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import  { defineComponent } from 'vue';
-import Timeline from './components/Timeline.vue';
-import Progress from "./components/Progress.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
-    Timeline,
-    Progress
+
   }
 })
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+      <Suspense>
+        <template #default>
+          <timeline/>
+        </template>
+        <template #fallback>
+          <Progress/>
+        </template>
+      </Suspense>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import  { defineComponent } from 'vue';
+import Timeline from '@/components/Timeline.vue';
+import Progress from "@/components/Progress.vue";
 
 export default defineComponent({
   name: 'Home',
   components: {
-  },
-});
+    Timeline,
+    Progress
+  }
+})
 </script>
+
+<style>
+
+</style>
+
