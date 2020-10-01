565af7edb79d151d41a2680b920aee58bc6477fd
huanshenyi <txy1226052@gmail.com>
2020-10-01 22:38:04 +0900

    booklistの右側のカード基本形完成

--- src/components/books/InfoCard.vue
+++ src/components/books/InfoCard.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="book-card">
-       card
+       <a href="" class="author">
+         <img src="http://s3.pstatp.com/toutiao/xitu_juejin_web/img/author.c5d975e.png" alt="">
+         <div class="items title">作者になる</div>
+       </a>
+       <div>|</div>
+       <a href="" class="feedback">
+         <img src="http://s3.pstatp.com/toutiao/xitu_juejin_web/img/feedback.1230fb5.png" alt="">
+         <div class="items title">バック</div>
+       </a>
     </div>
 </template>
 <script lang="ts">
@@ -15,6 +23,28 @@ export default defineComponent({
 </script>
 <style scoped>
 .book-card{
+  height: 120px;
   box-shadow: 0 1px 3px 0 rgba(4,5,13,.23);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.title{
+  margin-top: 8px;
+  font-size: 14px;
+}
+.items{
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #71777b;
+}
+.author{
+  padding-left: 40px;
+}
+.feedback{
+  padding-right: 40px;
 }
 </style>
