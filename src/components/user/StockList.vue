b87e2418856e33f19fe78220f2e6d6927743929a
huanshenyi <txy1226052@gmail.com>
2020-10-12 19:34:44 +0900

    ユーザーページのストック記事のレイアウト追加完了

---
+++ src/components/user/StockList.vue
@@ -0,0 +1,94 @@
+<template>
+    <!-- ユーザーがストックした記事 -->
+    <div class="item-list">
+        <div class="item" v-for="(item, index) in mokeList " :key="index">
+            <!-- 投稿者のicon -->
+            <figure class="userthmb image is-48x48">
+            <img src="https://lqzhgood.github.io/bulma-docs-cn/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+            <!-- 記事の基本データ -->
+            <div class="media_body">
+                <div class="item-link">username がxx/yyyy/mmに投稿しました。</div>
+                <div class="item-title">Golang + Gin + Dockerのホットリロード、VSCodeデバッグ環境の構築</div>
+                <div class="u-flex">
+                    <div class="tag-list">
+                        <span class="tag">Tag label</span>
+                        <span class="tag">Tag label</span>
+                        <span class="tag">Tag label</span>
+                    </div>
+                </div>
+            </div>
+            <!-- 記事のいいね、コメントなど -->
+            <ul class="item_link_status">
+              <li>
+                  <span class="not_checked">LGTM</span>
+                  4
+              </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+    name: "StockList",
+    components: {},
+    setup(){
+     const mokeList = ref([1,2,3,4]);
+     return { mokeList }
+    }
+})
+</script>
+<style scoped>
+.item{
+    display: flex;
+    padding: calc(1em*2/3);
+    font-size: 12px;
+    color: #444;
+    border-bottom: 1px solid #ddd;
+}
+.userthmb{
+    display: flex;
+    overflow: hidden;
+    border-radius: 50%;
+}
+.media_body{
+    flex: 1 1;
+    width: 0;
+    min-width: 0;
+}
+.item-link{
+    font-size: 1em;
+}
+.item-title{
+    display: block;
+    margin-bottom: .5em;
+    font-size: 1.3em;
+}
+.u-flex{
+    display: flex;
+}
+.tag-list{
+    align-items: center;
+    margin-left: 8px;
+    display: inline-flex;
+    flex-wrap: wrap;
+    font-size: 12px;
+}
+.tag {
+    margin: 0 1em .5em .5em;
+}
+.item_link_status{
+    padding-left: 0;
+    margin-bottom: 0;
+    font-size: 1.2em;
+    text-align: right;
+    list-style: none;
+}
+.not_checked{
+    display: inline-block;
+    width: 40px;
+    height: 9.1px;
+    fill: #999; 
+}
+</style>
--- src/components/user/UserActive.vue
+++ src/components/user/UserActive.vue
@@ -3,17 +3,19 @@
       <!-- 内容切替のタグ --> 
       <div class="tabs">
         <ul>
-            <li class="is-active"><a>アクティブ</a></li>
-            <li><a>記事</a></li>
+            <li class="is-active"><a>ストック一覧</a></li>
+            <li><a>マイ記事</a></li>
             <li><a>いいねした記事</a></li>
             <li><a >下書き一覧</a></li>
-            <li><a>ミニブック</a></li>
+            <li><a>ミニブック一覧</a></li>
         </ul>
       </div>
       <div class="card-content">
          <div class="content">
             <div class="activity-item">
-                <div class="source-header"></div>
+                <div class="source-header">
+                    <StockList/>
+                </div>
             </div>
          </div>
       </div>
@@ -21,9 +23,14 @@
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+
+import StockList from "@/components/user/StockList.vue";
+
 export default defineComponent({
     name: "UserActive",
-    components: {},
+    components: {
+        StockList
+    },
     setup() {
 
     }
